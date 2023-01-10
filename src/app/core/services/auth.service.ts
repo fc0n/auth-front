f1a8e04905498a5bbb8312e9328a7f5473b1ea9d
Jean Gabriel <jeangabrieljb9@gmail.com>
2023-01-10 18:49:10 -0300

    canActivate para canActivateChild para rotas filhas

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, map, Observable, throwError } from 'rxjs';
 
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -28,7 +30,16 @@ export class AuthService {
   }
 
   public logout(){
-    localStorage.removeItem('acess_token');
+    localStorage.removeItem('access_token');
     return this.router.navigate(['']);
+  }
+
+  public isAuthenticated(): boolean{
+    const token = localStorage.getItem('access_token');
+
+    if(!token) return false;
+
+    const jwtHelper = new JwtHelperService();
+    return !jwtHelper.isTokenExpired(token);
   }
 }
